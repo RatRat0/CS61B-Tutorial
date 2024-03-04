59223d0195859f4f13a42d04cedc3585eae63683
ratrat <962476394@qq.com>
2024-03-04 13:11:54 +0800

    proj0第一次提交

---
+++ proj0/Planet.java
@@ -0,0 +1,85 @@
+public class Planet {
+    static final double G = 6.67 * 0.00000000001;
+
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    public Planet(double xP, double yP, double xV,
+                  double yV, double m, String img) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+
+    public Planet(Planet p) {
+        xxPos = p.xxPos;
+        yyPos = p.yyPos;
+        xxVel = p.xxVel;
+        yyVel = p.yyVel;
+        mass = p.mass;
+        imgFileName = p.imgFileName;
+    }
+
+    public double calcDistance(Planet p) {
+        return Math.sqrt((this.xxPos - p.xxPos) * (this.xxPos - p.xxPos) + (this.yyPos - p.yyPos) * (this.yyPos - p.yyPos));
+    }
+
+    public double calcForceExertedBy(Planet p) {
+        double r = this.calcDistance(p);
+        return G * this.mass * p.mass /(r * r);
+    }
+
+    public double calcForceExertedByX(Planet p) {
+        double dx = p.xxPos - this.xxPos;
+        double F = calcForceExertedBy(p);
+        double r = calcDistance(p);
+        return F * dx / r;
+    }
+
+    public double calcForceExertedByY(Planet p) {
+        double dy = p.yyPos - this.yyPos;
+        double F = calcForceExertedBy(p);
+        double r = calcDistance(p);
+        return F * dy / r;
+    }
+
+    public double calcNetForceExertedByX(Planet[] allPlanets) {
+        double res = 0;
+        for (Planet p : allPlanets) {
+            if (!p.equals(this)) {
+                res += this.calcForceExertedByX(p);
+            }
+        }
+        return res;
+    }
+
+    public double calcNetForceExertedByY(Planet[] allPlanets) {
+        double res = 0;
+        for (Planet p : allPlanets) {
+            if (!p.equals(this)) {
+                res += this.calcForceExertedByY(p);
+            }
+        }
+        return res;
+    }
+
+    public void update(double dt, double fX, double fY) {
+        double aX = fX / this.mass;
+        double aY = fY / this.mass;
+        this.xxVel = this.xxVel + dt * aX;
+        this.yyVel = this.yyVel + dt * aY;
+        this.xxPos = this.xxPos + dt * this.xxVel;
+        this.yyPos = this.yyPos + dt * this.yyVel;
+    }
+
+    public void draw() {
+        StdDraw.picture(xxPos, yyPos, "images/" + imgFileName);
+    }
+}
