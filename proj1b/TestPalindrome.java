4a692a36f752a3d6e6b64937b6207c2d497f4d8f
ratrat <962476394@qq.com>
2024-03-06 13:05:15 +0800

    proj1b第二次提交

--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -5,8 +5,6 @@ public class TestPalindrome {
     // You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
-    static CharacterComparator offByOne = new OffByOne();
-    static CharacterComparator getOffBy5 = new OffByN(5);
 
     @Test
     public void testWordToDeque() {
@@ -32,20 +30,22 @@ public class TestPalindrome {
 
     @Test
     public void testIsPalindromeOne() {
-        assertFalse(palindrome.isPalindrome("horse", offByOne));
-        assertFalse(palindrome.isPalindrome("cat", offByOne));
+        CharacterComparator cc = new OffByOne();
+        assertFalse(palindrome.isPalindrome("horse", cc));
+        assertFalse(palindrome.isPalindrome("cat", cc));
 
-        assertTrue(palindrome.isPalindrome("abbdccab", offByOne));
-        assertTrue(palindrome.isPalindrome("rbbecas", offByOne));
-        assertTrue(palindrome.isPalindrome("", offByOne));
+        assertTrue(palindrome.isPalindrome("abbdccab", cc));
+        assertTrue(palindrome.isPalindrome("rbbecas", cc));
+        assertTrue(palindrome.isPalindrome("", cc));
     }
 
     @Test
     public void testIsPalindrome5() {
-        assertFalse(palindrome.isPalindrome("horse", getOffBy5));
-        assertFalse(palindrome.isPalindrome("cat", getOffBy5));
+        CharacterComparator cc = new OffByN(5);
+        assertFalse(palindrome.isPalindrome("horse", cc));
+        assertFalse(palindrome.isPalindrome("cat", cc));
 
-        assertTrue(palindrome.isPalindrome("", getOffBy5));
-        assertTrue(palindrome.isPalindrome("afaf", getOffBy5));
+        assertTrue(palindrome.isPalindrome("", cc));
+        assertTrue(palindrome.isPalindrome("afaf", cc));
     }
 }
